ba00392b3b376f28d87e95c53afb8a25c5057384
Edenilson Junior <edenilsonju10@gmail.com>
2025-05-07 19:28:01 -0300

    inicializacao do projeto

---
+++ InventoryManager/src/test/java/edu/ifsp/inventorymanager/InventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifsp.inventorymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

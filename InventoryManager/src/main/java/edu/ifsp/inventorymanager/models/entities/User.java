8bb2fc1d5a2ec195818cf2411ddbef0534dbf37e
Edenilson Junior <edenilsonju10@gmail.com>
2025-05-07 20:28:28 -0300

    atualiza dependencia

--- InventoryManager/src/main/java/edu/ifsp/inventorymanager/models/entities/User.java
+++ InventoryManager/src/main/java/edu/ifsp/inventorymanager/models/entities/User.java
@@ -6,12 +6,10 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Data
-@AllArgsConstructor
 public class User {
 
     @Id
